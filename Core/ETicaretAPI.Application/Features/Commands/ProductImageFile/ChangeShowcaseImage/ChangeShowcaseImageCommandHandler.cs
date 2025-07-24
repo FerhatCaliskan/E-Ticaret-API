70d65c806fc135fe1ce8f8fddeef6ede464d90dd
Ferhat Çalışkan <ferhatclkn52@outlook.com>
2025-07-24 17:45:42 +0300

    Ürün listeleme & sayfalama part 1

---
+++ Core/ETicaretAPI.Application/Features/Commands/ProductImageFile/ChangeShowcaseImage/ChangeShowcaseImageCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using ETicaretAPI.Application.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.ProductImageFile.ChangeShowcaseImage
+{
+	public class ChangeShowcaseImageCommandHandler : IRequestHandler<ChangeShowcaseImageCommandRequest, ChangeShowcaseImageCommandResponse>
+	{
+		readonly IProductImageFileReadRepository _productImageFileReadRepository;
+
+		public ChangeShowcaseImageCommandHandler(IProductImageFileReadRepository productImageFileReadRepository)
+		{
+			_productImageFileReadRepository = productImageFileReadRepository;
+		}
+
+		public Task<ChangeShowcaseImageCommandResponse> Handle(ChangeShowcaseImageCommandRequest request, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
