70ec17ec4350f1f31c39ff0b49a45b3f89ee4d75
Ferhat Çalışkan <ferhatclkn52@outlook.com>
2025-08-01 13:36:22 +0300

    Sipariş Tamamlama Mail İşlemleri

--- Core/ETicaretAPI.Application/Abstractions/Services/IOrderService.cs
+++ Core/ETicaretAPI.Application/Abstractions/Services/IOrderService.cs
@@ -1,9 +1,4 @@
 ﻿using ETicaretAPI.Application.DTOs.Order;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ETicaretAPI.Application.Abstractions.Services
 {
@@ -12,6 +7,6 @@ namespace ETicaretAPI.Application.Abstractions.Services
 		Task CreateOrderAsync(CreateOrder createOrder);
 		Task<ListOrder> GetAllOrdersAsync(int page, int size);
 		Task<SingleOrder> GetOrderByIdAsync(string id);
-		Task CompleteOrderAsync(string id);
+		Task<(bool, CompletedOrderDTO)> CompleteOrderAsync(string id);
 	}
 }
